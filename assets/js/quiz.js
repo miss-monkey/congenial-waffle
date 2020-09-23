502174f215e0be0f6ba4fcf996edffabb431e99a
miss-monkey <justina278@gmail.com>
2020-09-23 11:24:34 +0000

    Added functionality to quiz answer buttons

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,22 +1,51 @@
-window.onload = function(){
-    $('.quiz').hide();
-    $('.start-btn').on('click', async function sendApiRequest() {
-    $('.front-page').hide();
-    $('.quiz').show();
-    let responce = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple`);
-    console.log(responce);
-    let data = await responce.json();
+window.onload = getData();
+async function getData() {
+    const responce = await fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple");
+    data = await responce.json();
     console.log(data);
     useApiData(data);
-    })
+}
 
-function useApiData(data){
-    document.querySelector('#category').innerHTML = `Category: ${data.results[0].category}`
-    document.querySelector('#difficulty').innerHTML = `Difficulty: ${data.results[0].difficulty}`
-    document.querySelector('#question').innerHTML = `${data.results[0].question}`
-    document.querySelector('#answer-a').innerHTML = `${data.results[0].correct_answer}`
-    document.querySelector('#answer-b').innerHTML = `${data.results[0].incorrect_answers[0]}`
-    document.querySelector('#answer-c').innerHTML = `${data.results[1].incorrect_answers[0]}`
-    document.querySelector('#answer-d').innerHTML = `${data.results[2].incorrect_answers[0]}`
+function useApiData(data) {
+    const question = data.results[0].question;
+    const difficulty = data.results[0].difficulty;
+    const answer1 = data.results[0].correct_answer;
+    const answer2 = data.results[0].incorrect_answers[0];
+    const answer3 = data.results[0].incorrect_answers[1];
+    const answer4 = data.results[0].incorrect_answers[2];
+
+   
+
+    // let allAnswers = [answer1, answer2, answer3, answer4];
+    // let choices = Array.from(document.getElementsByClassName('choice-text'));
+    
+
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("difficulty").innerHTML = `Difficulty: ${difficulty}`;
+    document.getElementById("choice1").innerHTML = answer1;
+    document.getElementById("choice2").innerHTML = answer2;
+    document.getElementById("choice3").innerHTML = answer3;
+    document.getElementById("choice4").innerHTML = answer4;
+
 }
-}
+
+$('div .a').click(function() {
+    $(this).children().addClass('green');
+    $('div .b, div .c, div .d').hide('slow');
+
+})
+
+$('div .b').click(function() {
+    $(this).children().addClass('red');
+    $('div .a, div .c, div .d').hide('slow');
+})
+
+$('div .c').click(function() {
+    $(this).children().addClass('red');
+    $('div .b, div .a, div .d').hide('slow');
+})
+
+$('div .d').click(function() {
+    $(this).children().addClass('red');
+    $('div .b, div .c, div .a').hide('slow');
+})
